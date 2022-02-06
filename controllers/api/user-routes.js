3cc1e42f2712a8804592666f3c2f33940fe5acd9
BTV <horsfalm@gmail.com>
2022-02-06 18:19:05 +1100

    login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const req = require('express/lib/request');
 const { User, Post, Vote, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
@@ -60,7 +59,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+      })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -84,8 +91,26 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
 });
 
 router.put('/:id', (req, res) => {
